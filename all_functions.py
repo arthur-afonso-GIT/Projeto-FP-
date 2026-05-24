6dea2b3dd6db9332bd987c543599d47476cda226
Arthur <arthurcabraltietzmann@gmail.com>
2026-05-24 11:01:40 -0300

    possível editar animal e pequenas correções

--- all_functions.py
+++ all_functions.py
@@ -1,20 +1,23 @@
 import csv
+import os
 
 def criar_dados_animais():
     with open("dados_animais.csv", "w", newline="", encoding="utf-8") as dados:
+        dado='id', 'nome', 'espécie', 'raça', 'idade', 'saúde', 'chegada', 'comportamento'
         dados=csv.writer(dados)
-        dados.writerow('id', 'nome', 'espécie', 'raça', 'idade', 'saúde', 'chegada', 'comportamento')
+        dados.writerow(dado)
 
 def criar_dados_atividades():
     with open("dados_atividades.csv", "w", newline="", encoding="utf-8") as dados:
+        dado='id', 'id da atividade', 'nome da atividade', 'data', 'responsável'
         dados=csv.writer(dados)
-        dados.writerow('id', 'id da atividade', 'nome da atividade', 'data', 'responsável')
+        dados.writerow(dado)
 
-def salvar_dados(dados_animais):
+def salvar_dados(dados_animal):
     #"dados_animais.csv" é o nome do arquivo /// "a" significa q vai adicionar no final e/ou criar o arquivo /// newline="" garante que a linha acabe de maneira correta /// encoding="utf-8" permite o uso de caracteres especiais
     with open("dados_animais.csv", "a", newline="", encoding="utf-8") as dados:
         dados=csv.writer(dados)
-        dados.writerow(dados_animais)
+        dados.writerow(dados_animal)
         #json.dump(dados_animais, dados, indent=4)
 
 def salvar_atividades(dados_atividades):
@@ -25,43 +28,27 @@ def salvar_atividades(dados_atividades):
 
 def carregar_dados():
     global dados_animais
-
-    try:
-        with open("dados_animais.csv", "r", encoding="utf-8") as dados:
-            dados_animais = csv.reader(dados)
-
-            dados_animais = {
-                int(linha[0]): linha[1] for linha in dados if linha
-            }
-
-    except FileNotFoundError:
-        dados_animais = {}
+    with open("dados_animais.csv", "r", encoding="utf-8") as dados:
+        dados_animais = list(csv.reader(dados))
+        return dados_animais
 
 def carregar_dados_atv():
     global dados_atividades
 
-    try:
-        with open("dados_atividades.csv", "r", encoding="utf-8") as dados:
-            dados_atividades = csv.reader(dados)
-
-            dados_atividades = {
-                int(linha[0]): linha[1] for linha in dados if linha
-            }
-
-    except FileNotFoundError:
-        dados_atividades = {}
+    with open("dados_atividades.csv", "r", encoding="utf-8") as dados:
+        dados_atividades = list(csv.reader(dados))
 
 # item 1
-def cadastro_i(id,nome, especie, raca, idade, saude, chegada, comportamento):
-    if id in dados_animais:
+def cadastro_animal(id,nome, especie, raca, idade, saude, chegada, comportamento):
+    dados_animais=carregar_dados()
+    id_string=str(id)
+    if id_string in dados_animais:
         print("ID ja cadastrado")
         return
-    dados_animais=[id,nome, especie, raca, idade, saude, chegada, comportamento]
-    
+    dados_animal=[id, nome, especie, raca, idade, saude, chegada, comportamento]
+    salvar_dados(dados_animal)
     print("Cadastrado com sucesso!  ")
-    salvar_dados(dados_animais)
-    
-    return dados_animais    
+    dados_animais[id_string] = dados_animal   
 
 #item 2
 def cadastro_atividade(id,id_atv, nome_atv, data, responsavel):
@@ -75,24 +62,27 @@ def cadastro_atividade(id,id_atv, nome_atv, data, responsavel):
 
 # item 3
 def listar_animais():   
+    dados_animais=carregar_dados()
     if not dados_animais:
         print("Lista vazia. ")
         return
     
-    for i in dados_animais.items():
-        print(f" id: {i[0]}")
-        print(f" espécie: {i[1]}")
-        print(f" raça: {i[2]}")
-        print(f" Estado de saúde: {i[3]}")
-        print(f" comportamento: {i[4]}")
-        print(f" Nome: {i[5]}")
+    for i in dados_animais:
+        if i>0:
+            print(f" id: {i[0]}")
+            print(f" espécie: {i[1]}")
+            print(f" raça: {i[2]}")
+            print(f" Estado de saúde: {i[3]}")
+            print(f" comportamento: {i[4]}")
+            print(f" Nome: {i[5]}")
 
 #item 4
 def listar_atividades():
     if not dados_atividades:
         print("Lista vazia. ")
         return
-    for i in dados_atividades.items():
+    
+    for i in dados_atividades:
         print(f" id da atividade: {i[0]}")
         print(f" id do i: {i[1]}")
         print(f" Nome da atividade:{i[2]}")
@@ -101,7 +91,7 @@ def listar_atividades():
 
 # item 5
 def busca_especifica_nome(nome):
-    encontrados = []
+    encontrados = [[]]
 
     for dados in dados_animais.values():
         if dados['nome'].lower() == nome.lower():
@@ -120,36 +110,35 @@ def busca_especifica_nome(nome):
 
 # item 5
 def busca_especifica_id(id):
-    i = dados_animais.get(id)    
-    
-    if i:
-        print(f"Animal encontrado: {id}")
-        print(f" espécie: {i['especie']}")
-        print(f" raça: {i['raca']}")
-        print(f" Estado de saúde: {i['saude']}")
-        print(f" comportamento: {i['comportamento']}")
-        return True
+    for i in dados_animais:
+        if id in i:
+            print(f"Animal encontrado: {i[0]}")
+            print(f" espécie: {i[1]}")
+            print(f" raça: {i[2]}")
+            print(f" Estado de saúde: {i[3]}")
+            print(f" comportamento: {i[4]}")
+            return True
     else:
-        print(f"O i de id {id} não foi localizado")
+        print(f"O animal de id {id} não foi localizado")
         return False
 
 # item 6
 def busca_atv_id_i(id):
     for i in dados_atividades:
         if id in i:
-            print(f" ID da atividade: {i}")
-            print(f" Nome da atividade:{i['nome_atv']}")
-            print(f" Data da atividade:{i['data']}")
-            print(f" Nome do responsável:{i['responsavel']}")
+            print(f" ID da atividade: {i[0]}")
+            print(f" Nome da atividade:{i[1]}")
+            print(f" Data da atividade:{i[2]}")
+            print(f" Nome do responsável:{i[3]}")
             ciclos+=1
     if ciclos==0:
         print("id não existe\n")
 
 # item 6
-def busca_atv_id_atividade(id_atv):
+def busca_atv_nome_atividade(nome_atv):
     for i in dados_atividades:
-        if id_atv in i:
-            print(f" ID do i: {i[0]}")
+        if nome_atv in i:
+            print(f" ID do do animal: {i[0]}")
             print(f" Nome da atividade:{i[1]}")
             print(f" Data da atividade:{i[2]}")
             print(f" Nome do responsável:{i[3]}")
@@ -158,35 +147,32 @@ def busca_atv_id_atividade(id_atv):
         print("id não existe\n")
     
 # item 7
-def editar_i(id):
-    i = dados_animais.get(id)
-    if i:
-        print(f"Editando dados do i: {i['nome']} (ID: {id})")
-        print("Pressione Enter para manter o valor atual.")
-        
-        novo_nome = input(f"Novo nome [{i['nome']}]: ") or i['nome']
-        nova_especie = input(f"Nova espécie [{i['especie']}]: ") or i['especie']
-        nova_raca = input(f"Nova raça [{i['raca']}]: ") or i['raca']
-        nova_idade = input(f"Nova idade [{i['idade']}]: ") or i['idade']
-        nova_saude = input(f"Novo estado de saúde [{i['saude']}]: ") or i['saude']
-        novo_comportamento = input(f"Novo comportamento [{i['comportamento']}]: ") or i['comportamento']
+def editar_animal(id,nome, especie, raca, idade, saude, chegada, comportamento):
+    temp_dados_animais=[]
+    string_id=str(id)
+    for i in dados_animais:
+        if i[0]==string_id:
+            if nome!=0:
+                i[1]=nome
+            if especie!=0:
+                i[2]=especie
+            if raca!=0:
+                i[3]=raca
+            if idade!=0:
+                i[4]=idade
+            if saude!=0:
+                i[5]=saude
+            if chegada!=0:
+                i[6]=chegada
+            if comportamento!=0:
+                i[7]=comportamento
+        temp_dados_animais.append(i)
+    with open('dados_animais', 'w', newline="", encoding="utf-8") as arq:
+        escritor = csv.writer(arq)
+        escritor.writerows(temp_dados_animais)
 
-        dados_animais[id] = {
-            'nome': novo_nome,
-            'especie': nova_especie,
-            'raca': nova_raca,
-            'idade': nova_idade,
-            'saude': nova_saude,
-            'chegada': i['chegada'],
-            'comportamento': novo_comportamento,
-            'id': id
-        }
-        
-        print(f"\nSucesso: O cadastro do i {id} foi atualizado!")
-    else:
-        print(f"Erro: O i de ID {id} não foi encontrado para edição.")
-    salvar_dados()
-    return dados_animais
+
+
 
 # item 9
 def excluir_i(id):
@@ -211,12 +197,12 @@ def menu():
         opcao = input("""
 O que você deseja fazer?
 
-1 - Cadastrar i
+1 - Cadastrar animal
 2 - Cadastrar atividades
 3 - Listar animais
 4 - Listar atividades
-5 - Buscar i por ID
-6 - Buscar atividade por i
+5 - Buscar animal por ID
+6 - Buscar atividade por animal
 7 - Editar cadastro
 8 - Editar atividade
 9 - Excluir cadastro
@@ -230,36 +216,33 @@ Escolha uma opção: \n """)
             # id, nome, especie, raca, idade, saude, chegada, comportamento
             while True:
                 try:
-                    id = int(input("Digite o id do i: "))
-                    nome = str(input("Digite o nome do i: "))
-                    especie = str(input("Digite a especie do i: "))
-                    raca = str(input("Digite a raça do i: "))
-                    idade = int(input("Digite a idade do i: "))
-                    saude = str(input("Digite o estado de saude do i: "))
-                    chegada = str(input("Digite a data de chegada do i (formato dd/mm/aaaa): "))
-                    comportamento = str(input("Digite o comportamento do i: "))
-
-                    cadastro_i(
-                        id,
-                        nome,
-                        especie,
-                        raca,
-                        idade,
-                        saude,
-                        chegada,
-                        comportamento
-                    )
-
+                    id = int(input("Digite o id do animal: "))
+                    nome = str(input("Digite o nome do animal: "))
+                    especie = str(input("Digite a especie do animal: "))
+                    raca = str(input("Digite a raça do animal: "))
+                    idade = int(input("Digite a idade do animal: "))
+                    saude = str(input("Digite o estado de saude do animal: "))
+                    chegada = str(input("Digite a data de chegada do animal (formato dd/mm/aaaa): "))
+                    comportamento = str(input("Digite o comportamento do animal: "))
                     break
-
                 except ValueError:
                     print("Algum dos dados inseridos foi invalido, tente novamente")
                     continue
+                cadastro_animal(
+                    id,
+                    nome,
+                    especie,
+                    raca,
+                    idade,
+                    saude,
+                    chegada,
+                    comportamento
+                )
 
         elif opcao=='2' or 'cadastrar atividades' in opcao.lower():
             while True:
                 try:
-                    id = int(input("Digite o id do i: "))
+                    id = int(input("Digite o id do animal: "))
                     id_atv = int(input("Digite o id da atividade: "))
                     nome_atv = str(input("Digite o nome da atividade: "))
                     data = str(input("Digite a data de realização do serviço (formato dd/mm/aaaa):"))
@@ -275,7 +258,7 @@ Escolha uma opção: \n """)
             listar_animais()
         elif opcao == '4' or 'lista atividades' in opcao.lower():
             listar_atividades()
-        elif opcao == '5' or 'buscar i' in opcao.lower() or 'id i' in opcao.lower():
+        elif opcao == '5' or 'buscar animal' in opcao.lower() or 'id animal' in opcao.lower():
             while True:
                 escolha=input("tipo de busca:\n1-nome\n2-ID")
                 if escolha == 1 or escolha.lower() == "nome":
@@ -305,23 +288,21 @@ Escolha uma opção: \n """)
                     busca_especifica_nome(nome_buscado)
                     break
                 except ValueError:
-                    print("i não registrado")
+                    print("Animal não registrado")
                     continue
 
 
         elif opcao == '6' or 'buscar atividade' in opcao.lower() or 'id atividade' in opcao.lower():
             while True:
-                escolha=input("Deseja buscar por qual ID?\n1-i\n2-atividade")
-                if escolha == 1 or escolha.lower() == "i":
-                    escolha=0
+                escolha=input("forma de busca: \n1-ID do animal\n2-Nome da atividade")
+                if escolha == 1 or escolha.lower() == "id":
                     break
-                elif escolha == 2 or escolha.lower() == "id":
-                    escolha=1
+                elif escolha == 2 or escolha.lower() == "nome":
                     break
                 else:
                     print("valor inválido")
 
-            while escolha==0:
+            while escolha==1:
                 try:
                     id = int(input("Digite o ID: "))
                     break
@@ -332,19 +313,44 @@ Escolha uma opção: \n """)
 
             busca_atv_id_i(id)
 
-            while escolha == 1:
+            while escolha == 2:
 
                 try:
-                    id_atv = str(input("Digite o ID a ser buscado: "))
-                    busca_atv_id_atividade(id_atv)
+                    nome_atv = str(input("Digite o nome a ser buscado: "))
+                    busca_atv_nome_atividade(nome_atv)
                     break
                 except ValueError:
-                    print("ID não registrado")
+                    print("nome não registrado")
                     continue
 
-        elif opcao == '7' or 'editar' in opcao.lower():
+        elif opcao == '7' or 'editar animal' in opcao.lower():
             id = int(input("Digite o id do cadastro a ser alterado: "))
-            editar_i(id)
+            while True:
+                quant=int(input("Digite quantas alterações deseja fazer?"))
+                if quant>=1 and quant<=7:
+                    while quant>0:
+                        escolha=int(input(f"Quantidade de itens a alterar {quant}\n\nEscolha um item para o alterar\n1-Nome do animal\n2-Espécie do animal\n3-idade do animal\n4-Raça do animal\n5-Saúde do animal\n6-Data de chegada do animal\n7-Comportamento do animal"))
+                        if escolha==1:
+                            nome=input("Digite o novo nome: ")
+                        elif escolha==1:
+                            nome=input("Digite o novo nome: ")
+                        elif escolha==2:
+                            especie=input("Digite a nova espécie: ")
+                        elif escolha==3:
+                            idade=input("Digite a nova idade: ")
+                        elif escolha==4:
+                            nome=input("Digite o novo nome: ")
+                        elif escolha==5:
+                            nome=input("Digite o novo nome: ")
+                        elif escolha==6:
+                            nome=input("Digite o novo nome: ")
+                        elif escolha==7:
+                            nome=input("Digite o novo nome: ")
+                        else:
+                            print("escolha inválida")
+                    break
+#nome, especie, raca, idade, saude, chegada, comportamento,
+            editar_animal(id, nome, especie, raca, idade, saude, chegada, comportamento,)
 
         elif opcao == '9' or 'exclui' in opcao.lower():
             id = int(input('Digite o id do i a ser excluido: '))
